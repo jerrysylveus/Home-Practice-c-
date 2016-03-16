20039df2e91605590e49e5a29a58afb6e8dee5ee
Jerry Sylveus <jerrysylveus@jerry-sylveuss-macbook.local>
2016-03-16 18:04:35 -0400

    Initial Commit

---
+++ Home Practice c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Home Practice c++
+//
+//  Created by Jerry Sylveus on 3/16/16.
+//  Copyright © 2016 Jerry Sylveus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
